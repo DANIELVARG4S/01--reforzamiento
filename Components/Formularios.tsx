8848a5d2c4df81d40879b4e3a07e2036bc3bdd4e
Daniel Vargas <danniliho@gmail.com>
2023-12-30 17:06:44 -0600

    commit primero

--- Components/Formularios.tsx
+++ Components/Formularios.tsx
@@ -15,14 +15,14 @@ export const Formularios = () => {
             className='form-control'
             placeholder='Email'
             value={email}
-            onChange={ ({ target} ) => onChange(target.value, 'email')}
+            // onChange={ ({ target} ) => onChange(target.value, 'email')}
             />
 
             <input type="text"
             className='form_control mt-2 mb-2'
             placeholder='Password' 
             value={password}
-            onChange={ ({target} ) => onChange(target.value, 'password')}
+            // onChange={ ({target} ) => onChange(target.value, 'password')}
             />
 
             <code>
